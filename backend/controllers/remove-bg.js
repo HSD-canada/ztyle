7707eb4c1279732674e0da77157eea02dd4d79b5
Rewge <rewgey@gmail.com>
2025-01-24 14:46:04 -0800

    Implemented CreateJWT() & VerifyJWT()

---
+++ backend/controllers/remove-bg.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const child = require('child_process');
+
+function removeBg(req,res){
+    
+}
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const fs = require('fs'); //Read,create... files
 const path = require('path'); //Work with paths
 const child_process = require('child_process'); //Run tasks parallel and on different processes
 const url = require('url'); //Allows to parse url into diff objects
-//const {remove} = require('./controllers/removeBg');
+const {remove} = require('./controllers/controller-remove-bg');
 
 const PORT = 3000;
 
@@ -13,8 +13,7 @@ const server = http.createServer((req,res)=>{
     console.log("Request Incoming");
     const req_url = url.parse(req.url);
     if(req_url.pathname === '/remove-bg' && req.method === "POST"){
-        res.statusCode = 200;
-        res.end("API!");
+        remove(req,res);
     }
     else{
         res.statusCode = 400;
